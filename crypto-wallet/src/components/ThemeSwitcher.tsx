5d84a77688bad60e693d35b57978cea5e8f4ad2a
pouyabakhshi <pouyabakhshiinfo@gmail.com>
2024-08-28 21:51:16 +0330

    create ThemeSwitcher Component for switch theme in future

--- crypto-wallet/src/components/ThemeSwitcher.tsx
+++ crypto-wallet/src/components/ThemeSwitcher.tsx
@@ -1,5 +1,16 @@
-const ThemeSwitcher = () => {
-  return <div>ThemeSwitcher</div>;
+const ThemeSwitcher: React.FC = () => {
+  const toggleTheme = () => {
+    document.documentElement.classList.toggle("dark");
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-2 bg-gray-200 dark:bg-gray-700 rounded "
+    >
+      Toggle Theme
+    </button>
+  );
 };
 
 export default ThemeSwitcher;
